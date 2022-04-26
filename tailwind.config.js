496a26e214666d9ce84323b8b6c4f8c6e4236fe5
geovanejh <geovanehartmann@gmail.com>
2022-04-25 21:05:00 -0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,18 @@
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx}",
-        "./src/components/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            fontFamily: {
-                sora: ["Sora", "sans-serif"],
-            },
-        },
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora", "sans-serif"],
+      },
     },
-    variants: {
-        extend: {},
-    },
-    plugins: [],
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
